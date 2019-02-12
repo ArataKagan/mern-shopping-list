2bb3bab08f3fc71b03254472ab8cc1c382d632b3
Arata Kagan <aratakagan@gmail.com>
2019-02-12 13:05:14 -0800

    debug prod

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV === 'production'){
     // Set static folder 
     app.use('/static', express.static(path.join(__dirname, 'client/build'))); 
 
-    app.get('*', (req, res) => {
+    app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
